7c739913e1e9e3b9bd8b9bb32634a28119a06f52
zenliarth <zen.liarth@devsquad.com>
2023-06-17 19:05:33 -0300

    install laravel ide helper

--- app/Models/Attachment.php
+++ app/Models/Attachment.php
@@ -2,10 +2,38 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Models\Attachment
+ *
+ * @property int $id
+ * @property int $task_id
+ * @property string $name
+ * @property string $path
+ * @property string $type
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read Task $task
+ *
+ * @method static \Database\Factories\AttachmentFactory factory($count = null, $state = [])
+ * @method static Builder|Attachment newModelQuery()
+ * @method static Builder|Attachment newQuery()
+ * @method static Builder|Attachment query()
+ * @method static Builder|Attachment whereCreatedAt($value)
+ * @method static Builder|Attachment whereId($value)
+ * @method static Builder|Attachment whereName($value)
+ * @method static Builder|Attachment wherePath($value)
+ * @method static Builder|Attachment whereTaskId($value)
+ * @method static Builder|Attachment whereType($value)
+ * @method static Builder|Attachment whereUpdatedAt($value)
+ *
+ * @mixin \Eloquent
+ */
 class Attachment extends Model
 {
     use HasFactory;
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,11 +2,46 @@
 
 namespace App\Models;
 
+use Database\Factories\TaskFactory;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Models\Task
+ *
+ * @property int $id
+ * @property int $created_by
+ * @property string $title
+ * @property string $description
+ * @property int $completed
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property string|null $deleted_at
+ * @property-read Collection<int, \App\Models\Attachment> $attachments
+ * @property-read int|null $attachments_count
+ * @property-read User|null $createdBy
+ * @property-read string $attachment_url
+ *
+ * @method static TaskFactory factory($count = null, $state = [])
+ * @method static Builder|Task newModelQuery()
+ * @method static Builder|Task newQuery()
+ * @method static Builder|Task query()
+ * @method static Builder|Task whereCompleted($value)
+ * @method static Builder|Task whereCreatedAt($value)
+ * @method static Builder|Task whereCreatedBy($value)
+ * @method static Builder|Task whereDeletedAt($value)
+ * @method static Builder|Task whereDescription($value)
+ * @method static Builder|Task whereId($value)
+ * @method static Builder|Task whereTitle($value)
+ * @method static Builder|Task whereUpdatedAt($value)
+ *
+ * @mixin \Eloquent
+ */
 class Task extends Model
 {
     use HasFactory;
